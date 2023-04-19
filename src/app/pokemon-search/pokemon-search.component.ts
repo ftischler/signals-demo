4588b1337c421f93f686dc4b84da4581a05bf1a9
Florian Tischler <Florian.Tischler@gmx.net>
2023-04-19 23:03:21 +0200

    remove delay

--- src/app/pokemon-search/pokemon-search.component.ts
+++ src/app/pokemon-search/pokemon-search.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { IPokemon } from 'pokeapi-typescript';
-import { delay } from 'rxjs';
 
 @Component({
   selector: 'signals-pokemon-search',
@@ -22,7 +21,6 @@ export class PokemonSearchComponent {
     const pokemonName = this.searchTerm.toLowerCase();
     this.httpClient
       .get<IPokemon>(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-      .pipe(delay(5000))
       .subscribe((pokemon) => {
         this.pokemon = pokemon;
         console.log(`Successfully resolved Pokémon ${pokemonName}`);
